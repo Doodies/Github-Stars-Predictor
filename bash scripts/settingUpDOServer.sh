e8ccad4f3a1eede7fc8d5de23878f065b1072c00
hackbansu <bansal.pawan96@gmail.com>
2018-02-05 13:07:58 +0530

    updated setup file

--- bash scripts/settingUpDOServer.sh
+++ bash scripts/settingUpDOServer.sh
@@ -2,7 +2,7 @@ cd ./../node_scripts
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 npm install graphql-request dotenv csvtojson jsonfile
-echo "TOKEN = 2f2c1c7bdba256a4373388cdb5036ad9d0aa1792
+echo "TOKEN =
 START = 5
 END = 10" > .env
 cd ./../
