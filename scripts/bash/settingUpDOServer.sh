a2019ddf14b0c991736138c329c12834627b81c1
hackbansu <bansal.pawan96@gmail.com>
2018-02-17 16:24:16 +0530

    added DO_V3

--- scripts/bash/settingUpDOServer.sh
+++ scripts/bash/settingUpDOServer.sh
@@ -4,8 +4,7 @@ sudo apt-get install -y nodejs
 npm install
 echo "TOKEN =
 START = 0
-END = 10000
-LOOPDELAY = 1000" > .env
+END = 10000" > .env
 mkdir graphQlData
 cd ./../../
 cp .tmux.conf ~/.tmux.conf
